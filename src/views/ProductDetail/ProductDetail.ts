ad9f76e7ed16b39bd811b5d9ba3311f5cec62f63
Casper Engelmann <me@casperengelmann.com>
2018-12-07 11:04:13 +0100

    added quantity field

--- src/views/ProductDetail/ProductDetail.ts
+++ src/views/ProductDetail/ProductDetail.ts
@@ -1,16 +1,32 @@
 import { Component, Vue, Watch } from 'vue-property-decorator';
-import { Getter } from 'vuex-class';
+import { Getter, Action } from 'vuex-class';
 import { Route } from 'vue-router';
+
+export interface ProductData {
+  product_id: number;
+  quantity: number;
+  variation_id?: number;
+  variation?: any[];
+}
 
 @Component
 export default class ProductDetail extends Vue {
   @Getter public products!: any[];
+  @Action public addToCart!: Promise<any>;
 
   public product: any = {};
+  public quantity: number = 1;
   public $route!: Route;
 
   @Watch('products', { immediate: true })
   public onProductsChanged(value: any[], oldValue: any[]) {
     this.product = this.products.find((product) => product.id === Number(this.$route.params.id));
+  }
+
+  public clickAddToCart({ product_id }: ProductData) {
+    this.addToCart({
+      product_id,
+      quantity: this.quantity,
+    });
   }
 }
